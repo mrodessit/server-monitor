3e19506af3fcf9c119e39ddd936ca2861a3f5034
mrodessit <mrt@torba.com>
2016-12-20 15:58:04 +0200

    fix

--- routes/routes.js
+++ routes/routes.js
@@ -21,21 +21,5 @@ module.exports = function(app, passport) {
 
 		res.render('index.ejs', page);        
 	});
-/*
-	app.get('/login', function(req, res) {
-		res.render('login.ejs');
-	});
 
-	// process the login form
-	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/',
-		failureRedirect : '/login',
-		failureFlash : true
-	}));
-
-	app.get('/logout', isAuth, function(req, res) {
-		req.logout();
-		res.redirect('/login');
-	});
-*/    
 };
